8ad3dc6e1c2b06647a11efedbd784683fa6c1afa
WEB Kotlas <maxim@web-kotlas.ru>
2021-02-22 19:28:46 +0300

    Redux & Thunk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import {createStoreCustom} from "./createStoreCustom";
+import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
 import {rootReducer} from "./redux/rootReducer";
+import {asyncIncrement, decrement, increment} from "./redux/actions";
 import './styles.css'
+
 
 const counter = document.getElementById('counter')
 const btnAdd = document.getElementById('add')
@@ -8,19 +11,23 @@ const btnSub = document.getElementById('sub')
 const btnAsync = document.getElementById('async')
 const btnTheme = document.getElementById('theme')
 
-let store = createStoreCustom(rootReducer, 0);
+let store = createStore(
+    rootReducer,
+    0,
+    applyMiddleware(thunk)
+);
 
 
 btnAdd.addEventListener('click', () => {
-    store.dispatch({ type:'INCREMENT' })
+    store.dispatch(increment())
 })
 
 btnSub.addEventListener('click', () => {
-    store.dispatch({ type:'DECREMENT' })
+    store.dispatch(decrement())
 })
 
 btnAsync.addEventListener('click', () => {
-
+    store.dispatch(asyncIncrement())
 })
 
 
